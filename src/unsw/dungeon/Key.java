74cb991167583cf19e693c48159d0e8897453ea5
frank merriman <f.merriman@unsw.edu.au>
2020-07-21 04:57:46 +1000

    fixed more javadoc in key

--- src/unsw/dungeon/Key.java
+++ src/unsw/dungeon/Key.java
@@ -2,10 +2,19 @@ package unsw.dungeon;
 
 import java.util.List;
 
+/**
+ * A entity that can be used by the player to open a specific door
+ * @author Frank Merriman, The Tran
+ */
 public class Key extends Entity {
     private int u_id;
     
-
+    /**
+     * Creates a new key with a unique id at given location
+     * @param x axis location
+     * @param y axis location
+     * @param u_id unique id(corresponds to a door)
+     */
     public Key(int x, int y, int u_id) {
         super(x, y);
         this.u_id = u_id;
@@ -27,6 +36,12 @@ public class Key extends Entity {
         }
     }
 
+    /**
+     * Checks a players inventory for any existing keys
+     * to see if there is room for this key
+     * @param inventory of player we are checking
+     * @return true if inventory has no key else false
+     */
     private boolean canPickUpKey(List<Entity> inventory) {
         for (Entity e : inventory) {
             if (e instanceof Key) {

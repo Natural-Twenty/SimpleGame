bf0387dac45e9ed9299f6cb97e76de18dd5559fe
Natural-Twenty <z5075710@cse.unsw.edu.au>
2020-07-21 03:01:23 +1000

    Added javadoc

--- src/unsw/dungeon/Subject.java
+++ src/unsw/dungeon/Subject.java
@@ -1,7 +1,25 @@
 package unsw.dungeon;
-
+/**
+ * Subject interface
+ * 
+ * An interface for subjects to be observed by Observers.
+ * 
+ * @author Frank Merriman, The Tran
+ */
 public interface Subject {
+    /**
+     * Add an observer to the subject.
+     * @param o An observer to add
+     */
+
     public void attach(Observer o);
+    /**
+     * Remove an observer from a subject.
+     * @param o An observer to remove
+     */
     public void detach(Observer o);
+    /**
+     * Notify all observers that are attached to subject
+     */
     public void updateObservers();
 }
--- src/unsw/dungeon/Weapon.java
+++ src/unsw/dungeon/Weapon.java
@@ -1,7 +1,20 @@
 package unsw.dungeon;
 
+/**
+ * Weapon interface 
+ * 
+ * An interface for all weapons
+ * 
+ * @author Frank Merriman, The Tran
+ */
 public interface Weapon {
-
+    /**
+     * Checks whether a weapon has no uses remaining
+     * @return True if weapon has no uses. Otherwise, false
+     */
     public boolean isBroken();
+    /**
+     * Use the weapon, taking away one of its remaining uses
+     */
     public void useWeapon();
 }

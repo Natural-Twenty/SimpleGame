37dbb9303aac5fb202cafbc253323f8a99baf918
frank merriman <f.merriman@unsw.edu.au>
2020-07-21 02:53:40 +1000

    added javadoc to some interfaces

--- src/unsw/dungeon/MoveBehaviour.java
+++ src/unsw/dungeon/MoveBehaviour.java
@@ -1,8 +1,28 @@
 package unsw.dungeon;
 
+
+/**
+ * Interface for allowing an object to implement certain methods
+ * associated with moving in a 2D space
+ * @author Frank Merriman, The Tran
+ *
+ */
 public interface MoveBehaviour {
 
+
+    /**
+     * Given two ints, moves object implementing MoveBehaviour to
+     * those respective coordinates
+     * @param x x axis value
+     * @param y y axis value
+     */
     public void moveTo(int x, int y);
+
+    /**
+     * Given a target destination via two ints, checks if object
+     * implementing MoveBehaviour can move to those respective coordinates
+     * @return true if object can enter those coordinates otherwise false
+     */
     public boolean canMove(int x, int y);
 
 }

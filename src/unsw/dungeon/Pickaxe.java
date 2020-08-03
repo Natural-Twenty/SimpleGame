49dd54825c218982e05bcbcb75eb745af519a978
Natural-Twenty <z5075710@cse.unsw.edu.au>
2020-08-03 12:07:32 +1000

    Added pickaxe class

---
+++ src/unsw/dungeon/Pickaxe.java
@@ -0,0 +1,32 @@
+package unsw.dungeon;
+
+public class Pickaxe extends Entity implements Weapon {
+    private int durability;
+
+    public Pickaxe(int x, int y) {
+        super(x,y);
+        this.durability = 5;
+    }
+
+    public void useWeapon() {
+        durability--;
+    }
+
+    public boolean isBroken() {
+        if (durability <= 0) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void onCollide(Entity e) {
+        if (e instanceof Player) {
+            Player player = (Player) e;
+            if (player.hasPickaxe() == false) {
+                player.equip(this);
+                setDisplayOnScreen(false);
+            }
+        }
+    }
+}
